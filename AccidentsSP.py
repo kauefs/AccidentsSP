632520b7d034d8fc3a0391d4c67fbaa5301e620d
KAUE <139394184+kauefs@users.noreply.github.com>
2024-04-03 09:38:17 -0300

    AccidentsSP.py

--- AccidentsSP.py
+++ AccidentsSP.py
@@ -39,7 +39,11 @@ st.markdown('''
 [![Python](https://img.shields.io/badge/Python-3-blue.svg)](https://www.python.org/)
 [![License](https://img.shields.io/badge/License-Apache_2.0-black.svg)](https://www.apache.org/licenses/LICENSE-2.0)
             ''')
-st.write('13 December 2023')
+with st.container():
+     cols = st.columns(3)
+     with cols[0]:st.empty()
+     with cols[1]:st.write('13 December 2023')
+     with cols[2]:st.empty()
 st.markdown('''
 Tens of thousands of Brazilians loose their lives in the country roads every year.
             ''')
