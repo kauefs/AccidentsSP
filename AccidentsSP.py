aaf37a80e8cdcd041c76cd3b34a35927bebd1245
KAUE <139394184+kauefs@users.noreply.github.com>
2025-02-05 17:50:40 -0300

    AccidentsSP.py

--- AccidentsSP.py
+++ AccidentsSP.py
@@ -33,27 +33,27 @@ df          = load_data()
 df['date']  = pd.to_datetime(df['date'])
 accidents   = df['date'].dt.year.value_counts().sort_index()
 # SIDE:
-st.sidebar.title(  'ƊⱭȾɅViƧi🧿Ƞ&trade;')
-st.sidebar.divider(              )
-st.sidebar.title(  'DashBoard'   )
+st.sidebar.title    ('ƊⱭȾɅViƧi🧿Ƞ&trade;')
+st.sidebar.divider  (                     )
+st.sidebar.title    ('DashBoard'          )
 st.sidebar.bar_chart(accidents, height=200)#color='#00BFFF'
-st.sidebar.write('Map Options:')
+st.sidebar.write    ('Map Options:'       )
 D3          = st.sidebar.empty()
 D2          = st.sidebar.empty()
 ano         = st.sidebar.slider('Year:',    2007, 2020, 2015)
 FilteredDF  = df[(df.date.dt.year == ano)]
-st.sidebar.info( ' {} Accidents'.format(FilteredDF.shape[0]))
+st.sidebar.info    ( ' {} Accidents'.format(FilteredDF.shape[0]))
 table       = st.sidebar.empty()
-st.sidebar.divider(            )
+st.sidebar.divider (           )
 st.sidebar.markdown('''
-![2023.12.13](  https://img.shields.io/badge/2023.12.13-000000)
+![2023.12.13   ](https://img.shields.io/badge/2023.12.13-000000)
 
-[![GitHub](     https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
-[![Medium](     https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
-[![LinkedIn](   https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
-[![Python](     https://img.shields.io/badge/3-646464?logo=python&logoColor=FFDE57&labelColor=4584B6)](https://www.python.org/)
+[![GitHub      ](https://img.shields.io/badge/GitHub-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
+[![Medium      ](https://img.shields.io/badge/Medium-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
+[![LinkedIn    ](https://img.shields.io/badge/LinkedIn-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
+[![Python      ](https://img.shields.io/badge/Python3-646464?logo=python&logoColor=FFDE57&labelColor=4584B6)](https://www.python.org/)
 
-[![License](    https://img.shields.io/badge/Apache--2.0-D22128?&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71)](https://www.apache.org/licenses/LICENSE-2.0)
+[![License     ](https://img.shields.io/badge/Apache--2.0-D22128?&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71)](https://www.apache.org/licenses/LICENSE-2.0)
 
 [![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi🧿Ƞ&trade;-0065FF?style=plastic&logoColor=0065FF&label=&copy;2023&labelColor=0065FF)](https://datavision.one/)
                     ''')
@@ -93,9 +93,9 @@ if   D3.checkbox( '3D', value=True):
                                           effects     = None ,
                                           map_provider='carto',
                                           parameters  = None))
-     st.divider(                   )
-if   D2.checkbox( '2D', value=False):
-     st.subheader('2D MAP')
+     st.divider  (                 )
+if   D2.checkbox ('2D', value=False):
+     st.subheader('2D MAP'         )
      SP    =folium.Map(location=[-23.259505,-47.0628577], zoom_start=6.75,
                       tiles='OpenStreetMap',           prefer_canvas=True)
      map   = df.sample(frac=.025, random_state=0)
@@ -123,9 +123,9 @@ if   D2.checkbox( '2D', value=False):
 #     map.apply(dots, axis = 1)
 #     st_folium(SP)
 #     st.divider( )
-if   table.checkbox('DataFrame', value=True):
-     st.subheader(       'DATA')
-     st.markdown(f'''➡️ Showing {'**{}** accidents'.format(FilteredDF.shape[0])} in **{ano}**:''')
-     st.write(FilteredDF)
-     st.divider(        )
+if   table.checkbox(  'DataFrame', value=True):
+     st.subheader  (  'DATA')
+     st.markdown   (f'''➡️ Showing {'**{}** accidents'.format(FilteredDF.shape[0])} in **{ano}**:''')
+     st.write      (FilteredDF)
+     st.divider    (          )
 #st.toast('Accident!', icon='💥')
